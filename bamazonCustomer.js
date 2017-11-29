649f53cb2d1b928488bf6f6e5e97bde670f902a1
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-29 17:22:45 -0500

    testing second inquirer and database ping

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,3 +15,53 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+// Greeting
+console.log("Welcome to bamazon: your one stop shop for anything and everything!");
+
+// First Prompt
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "What can I do for you today?",
+      choices: ["I would like to shop for a product!", "I know what I want to buy, just let me buy."],
+      name: "initialPrompt"
+    }
+  ])
+  .then(function(inquirerResponse) {
+
+    var initial = inquirerResponse.initialPrompt;
+
+    switch (intial) {
+      case "I would like to shop for a product!":
+
+        break;
+      case "I know what I want to buy, just let me buy.":
+      // User input prompt
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              message: "Please enter the product ID of the product you would like to purchase.",
+              name: "userInput"
+            }
+          ])
+          .then(function(inquirerResponse) {
+            var id = inquirerResponse.userInput;
+
+            // Ping the database for the product
+            con.connect(function(err) {
+              if (err) throw err;
+              con.query("SELECT * FROM customers WHERE item_id = " + id, function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+              });
+            });
+          });
+        break;
+      default:
+        console.log("I'm sorry, I didn't quite get that...");
+
+    };
+  });
